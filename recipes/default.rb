22702a8a81a9cd0d422547fb3aa1834b2a60788d
Thomas Meeus <thomas.meeus@kunstmaan.be>
2012-10-31 10:04:50 +0100

    Switched to PIP-installation fix Quantal issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,5 @@
-apt_repository "glances" do
-  action :add
-  uri "http://ppa.launchpad.net/arnaud-hartmann/glances-stable/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "81240C6D"
-end
-
-package "glances" do
+package "python-pip build-essential python-dev" do
   action :upgrade
 end
+
+execute 'pip install Glances'
