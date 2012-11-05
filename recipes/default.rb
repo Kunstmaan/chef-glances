b34461f1d6f66a8fcb0d21fed9c700fadf4d30bc
Thomas Meeus <thomas.meeus@kunstmaan.be>
2012-11-05 13:08:30 +0100

    updated installation procudere for pip

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,6 @@
-package "python-pip build-essential python-dev" do
-  action :upgrade
+%w{python-pip build-essential python-dev}.each do |pkg|
+    package pkg do
+      action :upgrade
+    end
 end
-
 execute 'pip install Glances'
